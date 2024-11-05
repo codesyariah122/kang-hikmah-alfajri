ea0b398fa30e09828f0624752af8a77509ecbcbf
codesyariah122 <pujiermanto@gmail.com>
2024-11-05 23:34:11 +0700

    fix/meta

--- app.vue
+++ app.vue
@@ -19,7 +19,7 @@
     title: 'Kang Hikmah - AlTajrib',
     ogTitle: 'Kang Hikmah - AlTajrib',
     description: 'Konsultasi masalah titit kecil, masalah percintaan, modal kawin , modal usaha dan segala macam permasalahan keduniawian',
-    ogDescription: 'Masalah Anda Bukan Masalah Kami, Temukan pencerahan hanya bersama Kang Hikmah - AlTajrib',
+    ogDescription: 'Masalah Anda Bukan Masalah Kami, jangan menyerah karna kami akan Temukan pencerahan hanya bersama Kang Hikmah - AlTajrib',
     ogImage: 'https://raw.githubusercontent.com/codesyariah122/kang-hikmah-alfajri/refs/heads/main/public/kanjeng.png',
     twitterCard: 'summary_large_image',
   })
