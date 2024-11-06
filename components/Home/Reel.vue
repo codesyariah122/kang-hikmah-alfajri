6a953793c778775c127f5a3bbb8f457d96518d91
codesyariah122 <pujiermanto@gmail.com>
2024-11-06 11:40:17 +0700

    fix/responsive-iframe

--- components/Home/Reel.vue
+++ components/Home/Reel.vue
@@ -1,27 +1,26 @@
 <template>
-    <div class="flex flex-col items-center p-6 max-w-3xl mx-auto bg-white rounded-lg shadow-md">
-      <h2 class="text-2xl md:text-3xl font-bold text-gray-800 mb-4 text-center">Praktik Spiritual Kami</h2>
-      
-      <p class="text-gray-600 text-base md:text-lg mb-6 text-center">
-        Kami menjalankan berbagai praktik spiritual untuk mencapai keseimbangan batin dan kedamaian.
-        Dengan meditasi, pernapasan sadar, dan ritual refleksi diri, kami memperdalam perjalanan menuju
-        kedamaian dan pemahaman yang lebih mendalam.
-      </p>
-      
-      <!-- <video
-        class="w-full rounded-lg shadow-lg"
-        autoplay="true"
-        controls
-        src="/videos/video-content1.mp4"
-      >
-        Browser Anda tidak mendukung video.
-      </video> -->
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/HlyxIeEJmjo?si=WstxBC-MeNJR7eJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
-  </template>
+  <div class="flex flex-col items-center p-6 max-w-3xl mx-auto bg-white rounded-lg shadow-md">
+    <h2 class="text-2xl md:text-3xl font-bold text-gray-800 mb-4 text-center">Praktik Spiritual Kami</h2>
+    
+    <p class="text-gray-600 text-base md:text-lg mb-6 text-center">
+      Kami menjalankan berbagai praktik spiritual untuk mencapai keseimbangan batin dan kedamaian.
+      Dengan meditasi, pernapasan sadar, dan ritual refleksi diri, kami memperdalam perjalanan menuju
+      kedamaian dan pemahaman yang lebih mendalam.
+    </p>
+    
+    <!-- <video
+      class="w-full rounded-lg shadow-lg"
+      autoplay="true"
+      controls
+      src="/videos/video-content1.mp4"
+    >
+      Browser Anda tidak mendukung video.
+    </video> -->
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/HlyxIeEJmjo?si=WstxBC-MeNJR7eJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+</template>
 
-  
-  <style scoped>
-  /* Tidak diperlukan style tambahan dengan penggunaan TailwindCSS */
-  </style>
-  
+
+<style scoped>
+/* Tidak diperlukan style tambahan dengan penggunaan TailwindCSS */
+</style>
