a7fd85caa04fe723612cfb84c2190314c75f0524
codesyariah122 <pujiermanto@gmail.com>
2024-11-06 11:53:31 +0700

    fix/respo

--- components/Home/AboutUs.vue
+++ components/Home/AboutUs.vue
@@ -1,9 +1,25 @@
 <template>
-    <section id="about" class="py-20">
-      <div class="container mx-auto text-center">
-        <h2 class="text-4xl font-bold mb-8">Tentang Kami</h2>
-        <p class="text-lg">Kang Hikmah AlTajrib adalah praktisi dengan pengalaman dalam pengobatan alternatif, memberikan layanan spiritual untuk berbagai kebutuhan masyarakat.</p>
+  <section id="about" class="py-20">
+    <div class="container mx-auto text-center">
+      <h2 class="text-4xl font-bold mb-8">Tentang Kami</h2>
+      <p class="text-lg">
+        Saya memilih menjadi "Dukun"/Consultant Spritual, karena
+        dukun/Consultant Spritual selalu selangkah Lebih tinggi dari semua gelar
+        , apapun gelarnya Saat berhadapan dengan Dukun Consultant Spritual, Dia
+        akan bersimpuh.... 🔥 Kok gak pake gelar Ulama atau Ahli hikmah ❓
+        Karena orang-orang yang akan bersimpuh kepada ulama atau Al Hikmah
+        hanyalah orang-orang yang baik , orang-orang yang buruk tidak akan
+        mendatangi ulama , mereka akan lebih memilih mendatangi dukun... Oleh
+        karena itu , Saya "lebih memilih" baju perdukunan/Consultant Spritual ,
+        dalam "menyebarkan" Faham Para ulama... Karena dengan baju dukun, saya
+        bisa masuk merangsek ke berbagai macam golongan...
+      </p>
+    </div>
+
+    <div class="container mt-6 mb-12">
+      <div>
+        <img src="/images/service-1.jpeg" class="w-auto" alt="">
       </div>
-    </section>
-  </template>
-  
+    </div>
+  </section>
+</template>
