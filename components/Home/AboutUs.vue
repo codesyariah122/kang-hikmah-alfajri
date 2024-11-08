75447bab2eb7b5fdb793749e4d7f95f2f0af9b79
codesyariah122 <pujiermanto@gmail.com>
2024-11-08 15:07:27 +0700

    fix/resp

--- components/Home/AboutUs.vue
+++ components/Home/AboutUs.vue
@@ -21,7 +21,7 @@
         <img src="/images/service-1.jpeg" class="w-auto" alt="" />
       </div>
     </div>
-    <div class="flex mt-12 justify-center gap-6 w-screen">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-12 w-full max-w-screen-xl mx-auto">
       <HomeProductCard
         image="/images/sample1.jpeg"
         title="Minyak nyai lastri - Alfajri"
